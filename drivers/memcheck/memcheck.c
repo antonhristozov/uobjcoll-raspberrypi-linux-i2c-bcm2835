46faa6c12ceec46bc0038db12d347b7e36cc50f9
Anton Hristozov <adhristozov@sei.cmu.edu>
2020-11-19 15:33:49 -0500

    first working version that uses vm_map_range

--- drivers/memcheck/memcheck.c
+++ drivers/memcheck/memcheck.c
@@ -10,40 +10,50 @@
 #include <asm/types.h>
 #include <asm/fcntl.h>
 #include <linux/io.h>
+#include <linux/mm.h>
+#include <linux/pagemap.h>
 #include <asm/pgtable.h>
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Driver for testing virtual memory window");
 MODULE_VERSION("0.1");
 
-#define MEM_SIZE  1024
-static char *buf;
-static char *virt_buf;
+static char *remapped;
+static struct page** pages;
+#define MEM_SIZE (64*1024*1024)
+
+struct vmap_area *find_vmap_area(unsigned long addr);
 
 static int __init memcheck_init(void){
-   printk(KERN_INFO "memcheck module loaded.\n");
-   buf = kmalloc(MEM_SIZE,GFP_KERNEL); /* get 16384 bytes of physical memory */
-   if (!buf){
-      printk("memcheck kmalloc() failed\n");
-   }else{
-      printk("memcheck kmalloc() succeeded at: %p\n", buf);
-   }
-   strcpy(buf,"memcheck123"); 
+   unsigned long npages;
+   int nid;
+   int i;
+   npages =  MEM_SIZE/PAGE_SIZE;
 
-   virt_buf = NULL;
-   //virt_buf = (char *)ioremap_page_range(0,MEM_SIZE,(unsigned long)buf,PAGE_SHARED);
-   if(!virt_buf){
-      printk("memcheck remapping failed \n");
+   pages = vmalloc(npages * sizeof(struct page *));
+   for(i=0;i<npages;i++){
+      pages[i] = alloc_pages(GFP_KERNEL,0); /* Allocate 1 page */
+   }
+
+   nid = page_to_nid(pages[0]); // Remap on the same NUMA node.
+
+   remapped = vm_map_ram(pages, npages, nid, PAGE_KERNEL);
+   if(remapped != NULL){
+      printk(KERN_INFO "vm_map_range() succeeded %p\n",remapped);
    }
    else{
-      printk("memcheck remapping succeeded at: %p\n", virt_buf);
+      printk(KERN_INFO "vm_map_range() failed\n");
    }
+   printk(KERN_INFO "memcheck module loaded.\n");
+   printk(KERN_INFO "memory size: %d\n",MEM_SIZE);
+   printk(KERN_INFO "npages: %ld\n",npages);
+   printk(KERN_INFO "page size: %ld\n",PAGE_SIZE);
+
    return 0;
+
 }
 
 static void __exit memcheck_exit(void){
-   printk("buf contents: %s\n",buf);
-   kfree(buf);
    printk(KERN_INFO "memcheck module unloaded.\n");
 }
 
