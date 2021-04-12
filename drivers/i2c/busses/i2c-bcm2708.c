09522f04f646fd620d4aa791d3e621ae64529c36
Anton Hristozov <anton.hristozov@gmail.com>
2021-04-12 15:15:11 -0400

    Had to make updates for driver to compile

--- drivers/i2c/busses/i2c-bcm2708.c
+++ drivers/i2c/busses/i2c-bcm2708.c
@@ -37,6 +37,24 @@
 #include <linux/delay.h>
 
 #ifdef IOUOBJ
+typedef struct {
+        u32 r0;
+        u32 r1;
+        u32 r2;
+        u32 r3;
+        u32 r4;
+        u32 r5;
+        u32 r6;
+        u32 r7;
+        u32 r8;
+        u32 r9;
+        u32 r10;
+        u32 r11;
+        u32 r12;
+        //u32 r13;
+        //u32 r14;
+} arm8_32_regs_t;
+
 	#include <khcall.h>
 	#include <i2c-ioaccess.h>
 #endif //IOUOBJ
