2b9b73539ffb96fe6e45e07f8a97cc895867d5c5
Amit Vasudevan <amitvasudevan@acm.org>
2021-04-17 10:08:14 -0500

    i2c-bcm2708: temporarily switch writes to use native mmio access

--- drivers/i2c/busses/i2c-bcm2708.c
+++ drivers/i2c/busses/i2c-bcm2708.c
@@ -170,10 +170,10 @@ u32 __local_khcall_fast_retu32(u32 khcall_function, u32 param1, u32 param2) {
 #define __u_raw_writel __u_raw_writel
 static inline void __u_raw_writel(u32 val, volatile void __iomem *addr)
 {
-        //asm volatile("str %1, %0"
-        //             : : "Qo" (*(volatile u32 __force *)addr), "r" (val));
+        asm volatile("str %1, %0"
+                     : : "Qo" (*(volatile u32 __force *)addr), "r" (val));
 		//__local_khcall_fast(0,0,0);
-        __local_khcall_fast(UAPP_I2C_IOACCESS_WRITEL, (u32)addr, val);
+        //__local_khcall_fast(UAPP_I2C_IOACCESS_WRITEL, (u32)addr, val);
 }
 
 #define u_writel_relaxed(v,c)     __u_raw_writel((__force u32) cpu_to_le32(v),c)
